244fe397f6da5ef0b9dd8f80949c65c6e0bae0c1
R-A-Medeiros <r.medeiros1206@gmail.com>
2024-11-17 14:34:05 -0300

    Endpoint game by id

--- src/main/java/com/arcade/dslist/services/GameService.java
+++ src/main/java/com/arcade/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.arcade.dslist.dto.GameDTO;
 import com.arcade.dslist.dto.GameMinDTO;
 import com.arcade.dslist.repositories.GameRepository;
 
@@ -14,6 +16,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		var result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		var result = gameRepository.findAll();
 		var dto = result.stream().map(x -> new GameMinDTO(x)).toList()
